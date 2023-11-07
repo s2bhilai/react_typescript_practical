93fca13e19dfd690df7f9c000e198ccb29604915
Subin <subinbs4a@gmail.com>
2023-11-07 15:41:00 +0300

    adder Timer component integration with useContext hook

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,4 +1,4 @@
-import { Timer as TimerProps } from '../store/timers-context.tsx';
+import { Timer as TimerProps, useTimersContext } from '../store/timers-context.tsx';
 import Container from './UI/Container.tsx';
 import { useState,useEffect, useRef } from 'react';
 
@@ -7,6 +7,7 @@ export default function Timer({ name,duration }: TimerProps) {
   //Also unlike variables refs will not be created again when the component reloads 
   const interval = useRef<number | null>(null);
   const [remainingTime, setRemainingTime] = useState(duration * 1000);
+  const { isRunning } = useTimersContext();
 
   if(remainingTime < 0 && interval.current) {
     clearInterval(interval.current)
@@ -17,16 +18,21 @@ export default function Timer({ name,duration }: TimerProps) {
     //reload the component and again a new setInterval will be created and like this it will be on loop
     //In this case setInterval will be called only once and when the component re-loads 
     //setInterval will not be called again 
-    const timer =  setInterval(function() {
-      setRemainingTime((prevTime) => prevTime - 50);
-    },50);
+    let timer: number;
+    if (isRunning) {
+      timer =  setInterval(function() {
+        setRemainingTime((prevTime) => prevTime - 50);
+      },50);
 
-    interval.current = timer;
-
+      interval.current = timer;
+    } else if(interval.current) {
+      clearInterval(interval.current);
+    }
+    
     //Will get called just before useEffect is called 2nd time (every time before it runs again) or if the component is unmounted.
     //Bcoz we are using strict mode so react will call component 2 times.
     return () => clearInterval(timer);
-  },[]);
+  },[isRunning]);
 
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
