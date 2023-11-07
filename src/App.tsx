0bdf987a3ebdd06bf6ad66e97333584bcadd0539
Subin <subinbs4a@gmail.com>
2023-11-07 10:38:49 +0300

    useImperativeHandle to forward custom methods (manupulating DOMs) to parent components

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,23 @@
 //import Button from "./components/Button";
 //import Container from "./components/Container";
 import Input from "./components/Input";
-import Form from "./components/Form";
+import Form, {type FormHandle } from "./components/Form";
 import Button from "./components/Button";
+import { useRef } from "react";
 
 function App() {
+  const customForm = useRef<FormHandle>(null);
 
   function handleSave(data: unknown) {
     const extractedData = data as { name: string; age: string; };
     console.log(extractedData);
+
+    customForm.current?.clear();
   }
 
   return (
   <main>
-    <Form onSave={ handleSave }>
+    <Form onSave={ handleSave } ref={ customForm }>
       <Input type="text" label="Name" id="name" />
       <Input type="number" label="Age" id="age" />
       <p>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,22 +1,45 @@
 
-import { type ComponentPropsWithoutRef, type FormEvent } from 'react'
+import { useRef, type ComponentPropsWithoutRef, type FormEvent, 
+    useImperativeHandle, forwardRef } from 'react'
+
+export type FormHandle = { 
+    clear: () => void;
+}
 
 type FormProps = ComponentPropsWithoutRef<'form'> & {
     onSave: (value: unknown) => void;
 };
 
-export default function Form({ onSave,children,...otherProps }: FormProps) {
+const Form = forwardRef<FormHandle,FormProps>(function Form(
+    { onSave,children,...otherProps }: FormProps,
+    ref) {
+
+    const form = useRef<HTMLFormElement>(null);
+
+    useImperativeHandle(ref,() => {
+        return {
+           clear() {
+            console.log('CLEARING');
+            form.current?.reset();
+           } 
+        }
+    });//Works only with forwardRef
+
     function handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
 
         const formData = new FormData(event.currentTarget);
         const data = Object.fromEntries(formData);
         onSave(data);
+        //form.current?.reset(); //reset is a method on form, similarly expose a custom method 
+        //so that parent component can call that method, for this useImperativeHandle
     }
 
     return (
-        <form onSubmit={ handleSubmit } {...otherProps}>
+        <form onSubmit={ handleSubmit } {...otherProps} ref={form}>
             { children }
         </form>
     )
-}
+});
+
+export default Form;
